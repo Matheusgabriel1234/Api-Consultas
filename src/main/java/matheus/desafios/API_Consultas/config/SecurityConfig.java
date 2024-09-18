64b83d12919f2bad0412a95e7baf165fd003b9b6
Matheusgabriel1234 <magaje2005@gmail.com>
2024-09-18 14:11:40 -0300

    Docker and Spring security configuration

---
+++ src/main/java/matheus/desafios/API_Consultas/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package matheus.desafios.API_Consultas.config;
+
+import org.apache.coyote.http11.Http11InputBuffer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception {
+	https.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+	.formLogin((form) -> form.permitAll()).logout((logout) -> logout.permitAll());
+	
+	return https.build();
+	
+
+}
+	
+
+@Bean
+public UserDetailsService userDetailService() {
+	 UserDetails user = User.withDefaultPasswordEncoder()
+             .username("admin")
+             .password("y9X!vN2z&L4k@W8d")
+             .roles("USER")
+             .build(); 
+
+return new InMemoryUserDetailsManager(user);
+}
+}
---
+++ src/test/java/matheus/desafios/API_Consultas/ApiConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.desafios.API_Consultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
